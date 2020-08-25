0dfa521b4dd32bcea34bdf401b0cfefc56268865
Daniel Lopez <dlopez.10g@gmail.com>
2020-08-25 08:31:01 -0500

    change on home route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: 'admin/dashboard#index'
+  root to: 'admin/atentions#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
